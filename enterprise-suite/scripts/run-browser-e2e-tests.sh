bf6565845f6cdeb9f8dd7f696da504af02931f55
Danny Wei <katsutoxin@gmail.com>
2018-09-13 11:59:06 -0700

    Update script

--- enterprise-suite/scripts/run-browser-e2e-tests.sh
+++ enterprise-suite/scripts/run-browser-e2e-tests.sh
@@ -1,7 +1,9 @@
 # install nvm
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-source ~/.profile
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm version
 
 nvm install v9.4.0
