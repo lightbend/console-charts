3f5007ad7acc6e2ca54cf3df10a3b7b5f0160c27
Danny Wei <katsutoxin@gmail.com>
2018-09-19 14:36:59 -0700

    e2e test script

---
+++ enterprise-suite/scripts/run-e2e-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -exu
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# setup
+echo "Installing ES from helm charts, $( basename $( pwd ) )"
+
+make build
+make install-local
+
+# run tests
+echo "Running tests"
+cd $script_dir/../tests/e2e
+
