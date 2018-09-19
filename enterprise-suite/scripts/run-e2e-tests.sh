df3bc95b858f293b7f4dde2decb98df72d72857b
Danny Wei <katsutoxin@gmail.com>
2018-09-19 15:42:50 -0700

    Fix travis script

--- enterprise-suite/scripts/run-e2e-tests.sh
+++ enterprise-suite/scripts/run-e2e-tests.sh
@@ -14,17 +14,6 @@ make install-local
 echo "Running tests"
 cd $script_dir/../tests/e2e
 
-# install nvm
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm version
-
-nvm install v9.4.0
-nvm use v9.4.0
-
 # run the e2e test
 npm install
 npm run e2e:demo-app-setup
