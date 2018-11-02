50d5dad456d7ecf8dffc0c5ff6ce51770437f4a2
Andrius Mitkus <andrius.mitkus@gmail.com>
2018-11-02 12:22:54 +0200

    Add all cmdline args, cleanup run & execute

--- enterprise-suite/scripts/install-es.py
+++ enterprise-suite/scripts/install-es.py
@@ -11,52 +11,48 @@ from distutils.version import LooseVersion
 
 DEFAULT_TIMEOUT=3
 
-# User overridable variables and their defaults
-lightbend_commercial_username = None
-lightbend_commercial_password = None
-lightbend_commercial_credentials = '$HOME/.lightbend/commercial.credentials'
-es_repo='https://repo.lightbend.com/helm-charts'
-es_chart = 'enterprise-suite'
-es_namespace = 'lightbend'
-es_local_chart = None
-es_helm_name = 'enterprise-suite'
-es_force_install = False
-es_export_yaml = False
-dry_run = False
+# Parsed commandline args
+args = None
 
+# Runs a given command with optional timeout.
+# Returns (stdout, returncode) tuple. If timeout
+# occured, returncode will be negative (-9 on macOS).
 def run(cmd, timeout=None):
     stdout, stderr, returncode = None, None, None
     try:
-        proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if timeout != None:
-            timer = threading.Timer(timeout, proc.kill)
-            try:
-                timer.start()
-                stdout, stderr = proc.communicate()
-                returncode = proc.returncode
-            finally:
-                timer.cancel()
-        else:
-            stdout, stderr = proc.communicate()
-            returncode = proc.returncode
+        proc = subprocess.Popen(shlex.split(cmd),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        timer = threading.Timer(timeout, proc.kill) if timeout != None else None 
+        if timer != None:
+            timer.start()
+        stdout, stderr = proc.communicate()
+        returncode = proc.returncode
     finally:
+        if timer != None:
+            timer.cancel()
         return stdout, returncode
 
+# Executes a command if dry_run=False,
+# prints it to stdout, handles failure status
+# codes by exiting with and error if can_fail=False.
 def execute(cmd, can_fail=False):
     print(cmd)
-    if dry_run:
-        return 0
-    else:
+    if not args.dry_run:
         stdout, returncode = run(cmd)
         if not can_fail and returncode != 0:
             sys.exit("Command '" + cmd + "' failed:\n" + stdout)
         return returncode
+    return 0
 
 version_re = re.compile(r'([0-9]+\.[0-9]+(\.[0-9]+)?)')
 def require_version(cmd, required_version):
     # Use first word as a program name in messages
     name = cmd.partition(' ')[0]
-    stdout, returncode = run(cmd)
+
+    # Use 1s timeout, mainly for docker when DOCKER_HOST is unreachable
+    stdout, returncode = run(cmd, 1)
+
     if returncode == None:
         sys.exit("Required program '" + name + "' not found")
     elif returncode == 0 and stdout != '':
@@ -98,15 +94,15 @@ def preflight_check():
 
 def install_helm_chart(args):
     chart_ref = None
-    if es_local_chart != None:
+    if args.local_chart != None:
         # Install from local chart tarball
-        chart_ref = es_local_chart
+        chart_ref = args.local_chart
     else:
         execute('helm repo add es-repo ' + es_repo)
         execute('helm repo update')
         chart_ref = 'es-repo/' + es_chart
     
-    if es_export_yaml:
+    if args.export_yaml != 'false':
         # TODO
         pass
     else:
@@ -114,7 +110,7 @@ def install_helm_chart(args):
         should_upgrade = False
         stdout, returncode = run('helm status ' + es_helm_name, DEFAULT_TIMEOUT)
         if returncode == 0:
-            if es_force_install:
+            if args.force_install:
                 execute('helm delete --purge ' + es_helm_name)
                 print(('warning: helm delete does not wait for resources to be removed'
                        '- if the script fails on install, please re-run it.'))
@@ -126,21 +122,32 @@ def install_helm_chart(args):
         # TODO: add credentials, --set args
         if should_upgrade:
             execute('helm upgrade {} {}'
-                .format(es_helm_name, chart_ref))
+                .format(args.helm_name, chart_ref))
         else:
             execute('helm install {} --name {}'
-                .format(chart_ref, es_helm_name))
+                .format(chart_ref, args.helm_name))
 
 def setup_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--dry-run', help='only print out the commands that will be executed',
                         action='store_true')
+    parser.add_argument('--force-install', help='set to true to delete an existing install first, instead of upgrading',
+                        action='store_true')
+    parser.add_argument('--export-yaml', help='export resource yaml to stdout, forces dry_run if not false',
+                        choices=['creds', 'console', 'false'], default='false')
+    parser.add_argument('--helm-name', help='helm release name', default='enterprise-suite')
+    parser.add_argument('--local-chart', help='set to location of local chart tarball')
+    parser.add_argument('--namespace', help='namespace to install es-console into', default='lightbend')
+    parser.add_argument('--chart', help='chart name to install from the repository', default='enterprise-suite')
+    parser.add_argument('--repo', help='helm chart repository', default='https://repo.lightbend.com/helm-charts')
+    parser.add_argument('--creds', help='credentials file', default='~/.lightbend/commercial.credentials')
     parser.add_argument('--version', help='es-console version to install', type=str)
 
     return parser.parse_args()
 
 def main():
+    global args
     args = setup_args()
     print("DEBUG: args:" + str(args))
 
@@ -150,7 +157,7 @@ def main():
         print(("warning: --version has not been set, helm will use the latest available version. "
                "It is recommended to use an explicit version."))
     
-    #install_helm_chart(args)
+    install_helm_chart(args)
 
 if __name__ == '__main__':
     main()
