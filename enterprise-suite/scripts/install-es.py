f59af6b2dda4f8095a770045d1a23303f7764240
Andrius Mitkus <andrius.mitkus@gmail.com>
2018-11-05 10:51:44 +0200

    Credentials import & checking, do not assume running on minikube

--- enterprise-suite/scripts/install-es.py
+++ enterprise-suite/scripts/install-es.py
@@ -5,6 +5,7 @@ import os
 import shlex
 import subprocess
 import threading
+import tempfile
 import re
 import argparse
 from distutils.version import LooseVersion
@@ -17,16 +18,17 @@ args = None
 # Runs a given command with optional timeout.
 # Returns (stdout, returncode) tuple. If timeout
 # occured, returncode will be negative (-9 on macOS).
-def run(cmd, timeout=None):
+def run(cmd, timeout=None, stdin=None):
     stdout, stderr, returncode = None, None, None
     try:
         proc = subprocess.Popen(shlex.split(cmd),
                                 stdout=subprocess.PIPE,
+                                stdin=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
         timer = threading.Timer(timeout, proc.kill) if timeout != None else None 
         if timer != None:
             timer.start()
-        stdout, stderr = proc.communicate()
+        stdout, stderr = proc.communicate(input=stdin)
         returncode = proc.returncode
     finally:
         if timer != None:
@@ -69,21 +71,23 @@ def require_version(cmd, required_version):
     # Non-critical warning
     print("warning: unable to determine installed version of '" + name + "'")
 
-def preflight_check():
+def preflight_check(creds, minikube=False):
     # Check versions
     require_version("docker version -f '{{.Client.Version}}'", '17.06')
     require_version('kubectl version --client=true --short=true', '1.10')
     require_version('helm version --client --short', '2.10')
-    require_version('minikube version', '0.29')
+    if minikube:
+        require_version('minikube version', '0.29')
 
     # Check if kubectl is connected to a cluster. If not connected, version query will timeout.
     stdout, returncode = run('kubectl version', DEFAULT_TIMEOUT)
     if returncode != 0:
         sys.exit('Cannot reach cluster with kubectl')
 
-    # Check if docker is pointing to a cluster
-    if os.environ.get('DOCKER_HOST') == None:
-        sys.exit('Docker CLI is not pointing to a cluster. Did you run "eval $(minikube docker-env)"?')
+    if minikube:
+        # Check if docker is pointing to a cluster
+        if os.environ.get('DOCKER_HOST') == None:
+            sys.exit('Docker CLI is not pointing to a cluster. Did you run "eval $(minikube docker-env)"?')
 
     # Check if helm is set up inside a cluster
     stdout, returncode = run('helm version', DEFAULT_TIMEOUT)
@@ -92,15 +96,31 @@ def preflight_check():
 
     # TODO: Check if RBAC rules for tiller are set up
 
-def install_helm_chart(args):
+    # Check credentials
+    registry = 'lightbend-docker-commercial-registry.bintray.io'
+    stdout, returncode = run('docker login -u {} --password-stdin {}'.format(creds[0], registry),
+                             6, creds[1])
+    if returncode != 0:
+        print(stdout)
+        sys.exit('Unable to login to lightbend docker registry. Please check your credentials.')
+    else:
+        run('docker logout ' + registry, DEFAULT_TIMEOUT)
+    
+    # TODO: Try to pull docker image from lighbend registry
+    
+    # At the moment when I try 'docker pull {registry}/enterprise-suite/es-monitor-api:latest' I get:
+    # Error response from daemon: Get https://lightbend-commercial-registry.bintray.io/v2/: 
+    # x509: certificate is valid for *.bintray.com, bintray.com, not lightbend-commercial-registry.bintray.io
+
+def install_helm_chart(args, creds_file):
     chart_ref = None
     if args.local_chart != None:
         # Install from local chart tarball
         chart_ref = args.local_chart
     else:
-        execute('helm repo add es-repo ' + es_repo)
+        execute('helm repo add es-repo ' + args.repo)
         execute('helm repo update')
-        chart_ref = 'es-repo/' + es_chart
+        chart_ref = 'es-repo/' + args.chart
     
     if args.export_yaml != 'false':
         # TODO
@@ -108,10 +128,10 @@ def install_helm_chart(args):
     else:
         # Determine if we should upgrade or install
         should_upgrade = False
-        stdout, returncode = run('helm status ' + es_helm_name, DEFAULT_TIMEOUT)
+        stdout, returncode = run('helm status ' + args.helm_name, DEFAULT_TIMEOUT)
         if returncode == 0:
             if args.force_install:
-                execute('helm delete --purge ' + es_helm_name)
+                execute('helm delete --purge ' + args.helm_name)
                 print(('warning: helm delete does not wait for resources to be removed'
                        '- if the script fails on install, please re-run it.'))
             else:
@@ -126,6 +146,27 @@ def install_helm_chart(args):
         else:
             execute('helm install {} --name {}'
                 .format(chart_ref, args.helm_name))
+
+def write_temp_credentials(creds_tempfile, creds):
+    creds_tempfile.write("imageCredentials" +
+                    "    username: " + creds[0] +
+                    "    password: " + creds[1])
+    creds_tempfile.flush()
+
+def import_credentials(args):
+    creds = (os.environ.get('LIGHTBEND_COMMERCIAL_USERNAME'),
+             os.environ.get('LIGHTBEND_COMMERCIAL_PASSWORD'))
+
+    if creds[0] == None or creds[1] == None:
+        with open(os.path.expanduser(args.creds), 'r') as creds_file:
+            creds_dict = dict(re.findall(r'(\S+)\s*=\s*(".*?"|\S+)', creds_file.read()))
+            creds = (creds_dict.get('user'), creds_dict.get('password'))
+
+    if creds[0] == None or creds[1] == None:
+        sys.exit("Credentials missing, please check your credentials file\n"
+                 "LIGHTBEND_COMMERCIAL_CREDENTIALS=" + args.creds)
+
+    return creds
 
 def setup_args():
     parser = argparse.ArgumentParser()
@@ -149,15 +190,17 @@ def setup_args():
 def main():
     global args
     args = setup_args()
-    print("DEBUG: args:" + str(args))
+    creds = import_credentials(args)
 
-    preflight_check()
+    preflight_check(creds)
 
     if args.version == None:
         print(("warning: --version has not been set, helm will use the latest available version. "
                "It is recommended to use an explicit version."))
-    
-    install_helm_chart(args)
+
+    with tempfile.NamedTemporaryFile('w') as creds_tempfile:
+        write_temp_credentials(creds_tempfile, creds)
+        install_helm_chart(args, creds)
 
 if __name__ == '__main__':
     main()
