c472c844e872844559882c75fba2c91f1c96f4f4
David Brinegar <david.brinegar@lightbend.com>
2019-01-08 10:43:23 -0800

    revert

--- enterprise-suite/tests/e2e/cypress/support/cluster-details.ts
+++ enterprise-suite/tests/e2e/cypress/support/cluster-details.ts
@@ -1,19 +1,19 @@
 export class ClusterDetails {
     static infraContains(key: string, value: string) {
-        cy.get('rc-panel[title="Infrastructure"]', { timeout: 10000 })
-            .contains('.label-key', key, {timeout: 10000})
+        cy.get('lb-panel[title="Infrastructure"]', { timeout: 10000 })
+            .contains('.label-key', key, { timeout: 10000 })
             .parent()
-            .contains('.label-value', value, {timeout: 10000});
+            .contains('.label-value', value, { timeout: 10000 });
     }
 
-    static workloadHealthGte(severity: 'Healthy'|'Warning'|'Critical'|'Unknown', count: number) {
-        cy.get('rc-panel[title="Workload Health"]').contains(severity)
+    static workloadHealthGte(severity: 'Healthy' | 'Warning' | 'Critical' | 'Unknown', count: number) {
+        cy.get('lb-panel[title="Workload Health"]').contains(severity)
             .parent().children('.label-value').find('.right-count')
             .then(($dom) => {
                 const realCount = Number($dom.text());
                 expect(realCount).to.gte(count);
             }
-        );
+            );
     }
 
 
