4534a1ddfa77fab64d3c3102605e1370e37a213f
James Ravn <james.ravn@lightbend.com>
2019-01-08 13:55:50 +0000

    Fix cluster detail test to use rc-panel
    lb-panel doesn't exist.

--- enterprise-suite/tests/e2e/cypress/support/cluster-details.ts
+++ enterprise-suite/tests/e2e/cypress/support/cluster-details.ts
@@ -1,13 +1,13 @@
 export class ClusterDetails {
     static infraContains(key: string, value: string) {
-        cy.get('lb-panel[title="Infrastructure"]', { timeout: 10000 })
+        cy.get('rc-panel[title="Infrastructure"]', { timeout: 10000 })
             .contains('.label-key', key, {timeout: 10000})
             .parent()
             .contains('.label-value', value, {timeout: 10000});
     }
 
     static workloadHealthGte(severity: 'Healthy'|'Warning'|'Critical'|'Unknown', count: number) {
-        cy.get('lb-panel[title="Workload Health"]').contains(severity)
+        cy.get('rc-panel[title="Workload Health"]').contains(severity)
             .parent().children('.label-value').find('.right-count')
             .then(($dom) => {
                 const realCount = Number($dom.text());
