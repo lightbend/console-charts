f275c63cc1993f60705f38fe75fba6b29087fef5
Andrius Mitkus <andrius.mitkus@gmail.com>
2019-02-12 15:06:35 +0200

    Fix gotests docker scripts

--- scripts/console-test-openshift.sh
+++ scripts/console-test-openshift.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+cd console-charts
 oc login https://centralpark.lightbend.com --token=$1
 oc status && oc project && oc get pods
 GO111MODULE=on make -C enterprise-suite gotests-openshift NAMESPACE=console-backend-go-tests
