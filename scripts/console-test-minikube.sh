ddbf61583b2915848906420892e6cadae53aa51c
Andrius Mitkus <andrius.mitkus@gmail.com>
2019-02-12 14:44:44 +0200

    Fix gotests docker scripts

--- scripts/console-test-minikube.sh
+++ scripts/console-test-minikube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd console-charts
-minikube start --vm-driver=none
+minikube start --vm-driver=none --bootstrapper=localkube
 minikube addons enable ingress
 GO111MODULE=on make -C enterprise-suite install-helm gotests-minikube
