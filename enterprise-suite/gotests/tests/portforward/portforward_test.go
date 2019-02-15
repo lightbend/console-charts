f723f1dde79f60a6dd246f93eb7adc08e49490e6
Andrius Mitkus <andrius.mitkus@gmail.com>
2019-02-15 10:59:56 +0200

    Port port forwarding test from bash to go

---
+++ enterprise-suite/gotests/tests/portforward/portforward_test.go
@@ -0,0 +1,71 @@
+package ingress
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/lightbend/gotests/args"
+	"github.com/lightbend/gotests/testenv"
+
+	"github.com/lightbend/gotests/util"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const consoleRemotePort = 8080
+const consoleDeployment = "deployment/es-console"
+
+// Hardcode the local port because with random port setup it's currently
+// tricky to get cmd.StartAsync stdout without stopping it first.
+const localPort = 58363
+
+var portForwardCmd *util.CmdBuilder
+
+var _ = BeforeSuite(func() {
+	testenv.InitEnv()
+
+	portForwardCmd = util.Cmd("kubectl", "port-forward",
+		"-n", args.ConsoleNamespace,
+		consoleDeployment,
+		fmt.Sprintf("%d:%d", localPort, consoleRemotePort))
+	Expect(portForwardCmd.StartAsync()).To(Succeed())
+})
+
+var _ = AfterSuite(func() {
+	Expect(portForwardCmd.StopAsync()).To(Succeed())
+	testenv.CloseEnv()
+})
+
+var _ = Describe("all:portforward", func() {
+	It("forwards 127.0.0.1 requests to console", func() {
+		addr := fmt.Sprintf("http://127.0.0.1:%v", localPort)
+
+		err := util.WaitUntilSuccess(func() error {
+			resp, err := http.Get(addr)
+			if err != nil {
+				return err
+			}
+
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return err
+			}
+
+			if resp.StatusCode != 200 {
+				return fmt.Errorf("wanted 200 response, got %d: %s", resp.StatusCode, string(body))
+			}
+
+			return nil
+		})
+		Expect(err).To(Succeed())
+	})
+})
+
+func TestPortForward(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Port Forward Suite")
+}
