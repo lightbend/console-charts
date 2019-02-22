362d2c516b7a6635f2a75575d75daafbcf8c4472
marcoderama <marco.ariano@lightbend.com>
2019-02-21 19:34:15 -0700

    Change --unbind to --delete-pvcs

--- enterprise-suite/scripts/lbc.py
+++ enterprise-suite/scripts/lbc.py
@@ -399,7 +399,7 @@ def check_pv_usage(aboutToUninstall=False, namespace=None):
     #     # This case would be typical for a dev/demo.  Chances are we're okay losing the data.
     #     #    (Not sure how useful this is really.  Don't think they can (easily) grab the data.)
     #     printerr("WARNING: usePersistentVolumes was false. Continued (un)installation will result in the loss of Console data.")
-    #     fail("Stopping.  Invoke again with '--unbind' to proceed anyway, but save your data first if so desired")
+    #     fail("Stopping.  Invoke again with '--delete-pvcs' to proceed anyway, but save your data first if so desired")
     # elif
     if ((hasPVs and not wantsPVs) or (hasPVs and aboutToUninstall)):
         # Chance of losing real data here.
@@ -414,7 +414,7 @@ def check_pv_usage(aboutToUninstall=False, namespace=None):
             printerr("WARNING: Given the current and desired configs, continued (un)installation will result in the loss of Console data.")
             for pv in notRetainedPVs:
                 printerr("   info: Reclaim policy for PV {} for claim {} is not 'Retain'".format(pv[0], pv[1]))
-            printerr("Invoke again with '--unbind' to proceed anyway, but save your data first if so desired")
+            printerr("Invoke again with '--delete-pvcs' to proceed anyway, but save your data first if so desired")
             fail("Stopping")
 
         if len(retainedPVs) > 0:
@@ -538,7 +538,7 @@ def install(creds_file):
         if args.wait:
             helm_args += ' --wait'
 
-        if not args.unbind:
+        if not args.delete_pvcs:
             check_pv_usage()
 
         if should_upgrade:
@@ -559,7 +559,7 @@ def uninstall(status=None):
     elif status == 'deleting':
         fail('Unable to delete console installation {} - it is already being deleted'.format(args.helm_name))
     else:
-        if not args.unbind:
+        if not args.delete_pvcs:
             check_pv_usage(aboutToUninstall=True, namespace=namespace)
 
         printerr("info: deleting previous console installation {} with status '{}'".format(args.helm_name, status))
@@ -760,7 +760,7 @@ def setup_args(argv):
 
     # Common arguments for install and uninstall
     for subparser in [install, uninstall]:
-        subparser.add_argument('--unbind', help='ignore warnings about PVs and proceed anyway. CAUTION!',
+        subparser.add_argument('--delete-pvcs', help='ignore warnings about PVs and proceed anyway. CAUTION!',
                             action='store_true')
         subparser.add_argument('--dry-run', help='only print out the commands that will be executed',
                                action='store_true')
