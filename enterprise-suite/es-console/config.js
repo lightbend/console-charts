b7f3b486a774ced937502a6f135a5d28f1078e71
Danny Wei <katsutoxin@gmail.com>
2019-03-01 16:52:40 -0800

    Fix frontend e2e test

--- enterprise-suite/es-console/config.js
+++ enterprise-suite/es-console/config.js
@@ -1,7 +1,7 @@
 {{- /* Concatenates the key/values of the config into a list of JSON key=value, which are finally joined with ','. */ -}}
 {{- $settings := list }}
 {{- range $k, $v := .Values.consoleUIConfig }}
-{{- $settings = append $settings (printf "\"%s\"=\"%v\"" $k $v) }}
+{{- $settings = append $settings (printf "\"%s\":\"%v\"" $k $v) }}
 {{- end}}
 window.installedConfig = {
   {{ $settings | join "," }}
