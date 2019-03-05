785b9997e8290102e81c02b8715ac96b5e130dc6
Srikanth Koneru <srikanthops@users.noreply.github.com>
2019-03-05 11:22:41 -0800

    Remove kubernetes namespace label/srikanth (#318)
    * Remove generating kubernetes_namespace label
    * Add test to make none of the metrics have label kubernetes_namespace
    * increase timeout for http urls to 45 seconds

--- enterprise-suite/gotests/util/prometheus/prometheus.go
+++ enterprise-suite/gotests/util/prometheus/prometheus.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"reflect"
+	"time"
 
 	"github.com/lightbend/console-charts/enterprise-suite/gotests/util"
 )
@@ -32,8 +33,13 @@ type Connection struct {
 
 func (p *Connection) Query(query string) (*PromResponse, error) {
 	addr := fmt.Sprintf("%v/api/v1/query?query=%v", p.url, url.QueryEscape(query))
-
-	resp, err := http.Get(addr)
+	// Some of the tests with openshift clusters timed out in 20 seconds, so adding a timeout for 45 seconds.
+	timeout := time.Duration(45 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	// client.Get(url)
+	resp, err := client.Get(addr)
 	if err != nil {
 		return nil, err
 	}
