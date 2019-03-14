3663f67b14af1b8b6234e5d5f279042181d1efd3
Danny Wei <katsutoxin@gmail.com>
2019-03-14 12:34:13 -0700

    more reliable frontend e2e test (#344)
    * More assert to make test more reliable
    
    * Add visible check
    
    * Fix validation
    
    * Add assertion for metric selection visibility
    
    * Fix syntax
    
    * Some tweaks
    
    * Skip flaky test
    
    * Fix syntax
    
    * Test syntax
    
    * Fix syntax

--- enterprise-suite/tests/e2e/cypress/support/action.ts
+++ enterprise-suite/tests/e2e/cypress/support/action.ts
@@ -8,7 +8,11 @@ export class Action {
     }
 
     static editMonitor() {
+        cy.get('.form-container').contains('EDIT').should('not.have.attr', 'hidden');
+
         cy.get('.form-container').contains('EDIT').click();
+
+        cy.get('.form-container').contains('EDIT').should('have.attr', 'hidden');
     }
 
     static removeMonitor() {
