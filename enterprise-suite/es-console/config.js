b530b2761d4cdefa201a177f9c614e0cacbf858f
James Ravn <james@r-vn.org>
2019-03-15 18:00:34 +0000

    Fix console to install on helm 2.10+ (#346)
    * Use helm 2.10.0 for tests
    
    So we make sure console will install on helm 2.10.
    
    * Fix config.js generation

--- enterprise-suite/es-console/config.js
+++ enterprise-suite/es-console/config.js
@@ -1,8 +1,1 @@
-{{- /* Concatenates the key/values of the config into a list of JSON key=value, which are finally joined with ','. */ -}}
-{{- $settings := list }}
-{{- range $k, $v := .Values.consoleUIConfig }}
-{{- $settings = append $settings (printf "\"%s\":\"%v\"" $k $v) }}
-{{- end}}
-window.installedConfig = {
-  {{ $settings | join "," }}
-}
+window.installedConfig = {{ toJson .Values.consoleUIConfig }}
