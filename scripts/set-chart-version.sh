f03aa0f756f5c79283e516868aab6e9ce1f7ac63
James Ravn <james@r-vn.org>
2019-05-16 14:38:00 +0100

    Remove obsolete files and directories

---
+++ scripts/set-chart-version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eu
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+. $script_dir/lib.sh
+
+# script
+rel_dir=$( dirname $script_dir )
+chart_dir=$rel_dir/$1
+
+cd $chart_dir
+yq r Chart.yaml version
