8b66e9fd917c3f24b8e9c2179e7db2be509ef5bb
Andrius Mitkus <andrius.mitkus@gmail.com>
2019-06-19 15:41:23 +0300

    Make single instance scrape test less flaky

--- enterprise-suite/gotests/tests/prometheus/prometheus_test.go
+++ enterprise-suite/gotests/tests/prometheus/prometheus_test.go
@@ -141,7 +141,10 @@ var _ = Describe("all:prometheus", func() {
 	Context("kube-state-metrics", func() {
 		It("should only scrape a single instance", func() {
 			query := fmt.Sprintf(`count(kube_pod_status_ready{namespace="%s", es_workload="console-backend", condition="true"}) == 1`, args.ConsoleNamespace)
-			Expect(prom.HasData(query)).To(Succeed())
+			err := util.WaitUntilSuccess(util.SmallWait, func() error {
+				return prom.HasData(query)
+			})
+			Expect(err).To(Succeed())
 		})
 	})
 
