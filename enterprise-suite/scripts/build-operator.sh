2384042fa6a8b807a7861a7dafb5f085106d08b3
Andrius Mitkus <andrius.mitkus@gmail.com>
2019-08-22 15:32:16 +0300

    Scripts for installing operator-sdk and building console-operator

---
+++ enterprise-suite/scripts/build-operator.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+console_version="1.2.0"
+docker_registry="lightbend-docker-commercial-registry.bintray.io"
+image_name="enterprise-suite/console-operator"
+full_docker_name="${docker_registry}/${image_name}:${console_version}"
+
+operator-sdk new console-operator --type=helm --kind=LBConsole \
+    --api-version=lightbend.com/v1alpha1 --helm-chart-repo=https://repo.lightbend.com/helm-charts \
+    --helm-chart=enterprise-suite --helm-chart-version=${console_version}
+
+operator-sdk build ${full_docker_name}
+
+docker login -u $(DOCKER_USERNAME) -p $(DOCKER_PASSWORD) ${docker_registry}
+docker push ${full_docker_name}
