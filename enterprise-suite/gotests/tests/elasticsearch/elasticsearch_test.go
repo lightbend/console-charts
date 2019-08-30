d779f662fc6f8fc3a4448ab487054b6512a9e1fa
James Ravn <james@r-vn.org>
2019-08-30 15:11:41 +0100

    Fix all the things

--- enterprise-suite/gotests/tests/elasticsearch/elasticsearch_test.go
+++ enterprise-suite/gotests/tests/elasticsearch/elasticsearch_test.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/lightbend/console-charts/enterprise-suite/gotests/args"
 	"github.com/lightbend/console-charts/enterprise-suite/gotests/testenv"
 	"github.com/lightbend/console-charts/enterprise-suite/gotests/util/lbc"
 	"github.com/lightbend/console-charts/enterprise-suite/gotests/util/urls"
+	"github.com/lightbend/console-charts/enterprise-suite/gotests/util/kube"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -33,5 +37,13 @@ var _ = Describe("all:elasticsearch", func() {
 		addr := fmt.Sprintf("%v/%v", testenv.ConsoleAddr, "service/elasticsearch/")
 		_, err := urls.Get200(addr)
 		Expect(err).To(BeNil(), addr)
+	})
+
+	It("has data volumes owned by Elasticsearch", func() {
+		pods, err := testenv.K8sClient.CoreV1().Pods(args.ConsoleNamespace).
+			List(metav1.ListOptions{LabelSelector: "app.kubernetes.io/component=console-backend"})
+		Expect(err).To(BeNil())
+		pod := pods.Items[0]
+		Expect(kube.FileOwner(args.ConsoleNamespace, pod.Name, "elasticsearch", "/usr/share/elasticsearch/data")).To(Equal("1000"))
 	})
 })
