dd7320247befabdecee2fb9ee60eda37586ff189
James Ravn <james@r-vn.org>
2019-09-11 12:11:31 +0100

    Improve build-operator.sh
    Split out source and build, don't include redundant files.

--- operator/build-operator.sh
+++ operator/build-operator.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
 set -eu
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$script_dir"
@@ -14,24 +13,35 @@ full_docker_name="${docker_registry}/${image_name}:${VERSION}"
 
 echo "Building operator image ${full_docker_name}..."
 
-if [[ -n ${CONSOLE_TAG} ]]; then
+if [[ "$VERSION" == "latest" ]]; then
+    echo "Using local helm chart..."
+    new_operator_args=--helm-chart="$script_dir"/../enterprise-suite
+else
     echo "Using published ${CONSOLE_TAG} helm chart..."
     new_operator_args=--helm-chart-repo=https://repo.lightbend.com/helm-charts \
         --helm-chart=enterprise-suite --helm-chart-version="${CONSOLE_TAG}"
-else
-    echo "Using local helm chart..."
-    new_operator_args=--helm-chart=../enterprise-suite
 fi
 
-rm -rf console-operator
+rm -rf build && mkdir build
+
+cd build
 operator-sdk new console-operator --type=helm --kind=Console --api-version=${api_version} \
     ${new_operator_args}
 
 cd console-operator
 operator-sdk build "${full_docker_name}"
 
-sed -i "" "s#REPLACE_IMAGE#${full_docker_name}#g" deploy/operator.yaml
-sed -i "" "/Replace\ this\ with\ the\ built\ image\ name/d" deploy/operator.yaml
+sed -i'' "s#REPLACE_IMAGE#${full_docker_name}#g" deploy/operator.yaml
+sed -i'' "/Replace\ this\ with\ the\ built\ image\ name/d" deploy/operator.yaml
+
+# Create final manifests folder
+cd "$script_dir"
+rm -rf manifests && mkdir manifests
+cp -r build/console-operator/deploy/* manifests/
+cp -r src/* manifests/
+
+find manifests/
+echo "Done creating operator and manifests."
 
 # If --verify flag was given, check for git changes. Operator k8s resources should be commited, so
 # no diff will be produced after fresh operator build
